f85ee06b6c39aa1030c39b54c8dc41cb39396899
Emmett Steven <emmettsteven@Emmetts-MacBook-Pro.local>
2020-07-13 20:36:25 -0400

    Add ChakraUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 /* eslint-disable */
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Theme from './theme';
+import HomePage from './pages/HomePage';
 
 const App = () => (
-  <div>
-    Hello World.
-  </div>
+  <Theme>
+    <Route exact path="/" component={HomePage} />
+  </Theme>
 );
 
 export default App;
