d55c7f28358884c1cb35a97a94ca9cad59ab4944
Emmett Steven <emmettsteven@Emmetts-MacBook-Pro.local>
2020-07-16 16:43:01 -0400

    Built theme for rebass

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,40 +1,64 @@
 /* eslint-disable */
 import React from 'react';
-import ThemeProvider from '@chakra-ui/core/dist/ThemeProvider';
+import { ThemeProvider } from 'emotion-theming';
 
-const theme = {
-  breakpoints: ['30em', '48em', '62em', '80em'],
+const customTheme = {
+  breakpoints: ['40em', '52em', '64em'],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  colors: {
+    text: '#000',
+    background: '#fff',
+    palette: ["#e63946","#f1faee","#a8dadc","#457b9d","#1d3557"]
+  },
   fonts: {
-    heading: '"Avenir Next", sans-serif',
-    body: 'system-ui, sans-serif',
-    mono: 'Menlo, monospace',
+    default: 'Baskerville',
+    button: 'Roboto'
+  },
+  fontWeights: {
+    body: 400,
+    heading: 600
   },
   fontSizes: {
-    xs: '0.75rem',
-    sm: '0.875rem',
-    md: '1rem',
-    lg: '1.125rem',
-    xl: '1.25rem',
-    '2xl': '1.5rem',
-    '3xl': '1.875rem',
-    '4xl': '2.25rem',
-    '5xl': '3rem',
-    '6xl': '4rem',
+    xs: 12,
+    s: 14,
+    default: 16,
+    m: 20,
+    l: 24,
+    xl: 38,
+    huge: 48
   },
-  colors: {
-    50: '#f2f2f2',
-    100: '#d9d9d9',
-    200: '#bfbfbf',
-    300: '#a6a6a6',
-    400: '#8c8c8c',
-    500: '#737373',
-    600: '#595959',
-    700: '#404040',
-    800: '#262626',
-    900: '#0d0d0d',
+  radii: {
+    square: '0px',
+    round: '6px'
+  },
+  buttons: {
+    primary: {
+      borderRadius: 'round',
+      bg: 'main',
+      fontFamily: 'button',
+      ':hover': {
+        bg: 'palette.4'
+      }
+    },
+    secondary: {
+      borderRadius: 'round',
+      bg: 'text'
+    }
+  },
+  text: {
+    heading: {
+      fontFamily: 'default',
+      fontSize: 'huge',
+      color: 'background',
+      fontWeight: 'heading'
+    }
   }
-};
+}
 
-const Theme = (props) => <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+customTheme.colors.main = customTheme.colors.palette[3];
+customTheme.colors.light = customTheme.colors.palette[2];
+customTheme.colors.xLight = customTheme.colors.palette[1];
+customTheme.colors.error = customTheme.colors.palette[0];
 
+const Theme = (props) => <ThemeProvider theme={customTheme}>{props.children}</ThemeProvider>
 export default Theme;
