93a378b7b70b4a1fd9e1a0b69c7dbf5e21413631
Emmett Steven <emmett@draecodev.com>
2020-07-18 02:28:33 -0400

    Add custom modal component

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -26,3 +26,42 @@ export const Backdrop = (props) => (
 Backdrop.propTypes = {
   bgImg: PropTypes.string.isRequired
 };
+
+export const Modal = (props) => (
+  <div>
+    {
+      props.show
+        ? (
+          <Flex
+            sx={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              height: '100vh',
+              width: '100vw',
+              justifyContent: 'center',
+              alignItems: 'center',
+              bg: 'rgb(0,0,0,0.6)'
+            }}
+          >
+            <Box
+              sx={{
+                bg: '#fff',
+                minHeight: 200,
+                minWidth: 350,
+                borderRadius: 4,
+                boxShadow: '3px 3px 10px #000'
+              }}
+            >
+              {props.children}
+            </Box>
+          </Flex>
+        ) : null
+    }
+  </div>
+);
+
+Modal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  children: PropTypes.element.isRequired
+};
