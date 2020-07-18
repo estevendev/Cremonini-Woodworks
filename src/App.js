d7ea426cf4b81c850709b4f0b2513a0b0ff03eea
Emmett Steven <emmett@draecodev.com>
2020-07-17 20:13:34 -0400

    Remove NavBar from top level

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Theme from './theme';
-import NavBar from './components/NavBar';
-import logo from './img/cww_logo.png';
 import HomePage from './pages/HomePage';
-
-const navLinks = [
-  {
-    label: 'Projects',
-    href: '/#projects'
-  },
-  {
-    label: 'About',
-    href: '/#about'
-  },
-  {
-    label: 'Contact',
-    href: '/#contact'
-  }
-];
 
 const App = () => (
   <Theme>
-    <NavBar homeImg={logo} links={navLinks} />
     <Route exact path="/" component={HomePage} />
   </Theme>
 );
